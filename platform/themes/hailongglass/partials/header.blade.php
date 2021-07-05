01857ad92cd3401798c49305a859b6be2356ea71
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-07-05 15:13:47 +0700

    fix setup source

--- platform/themes/hailongglass/partials/header.blade.php
+++ platform/themes/hailongglass/partials/header.blade.php
@@ -20,4 +20,31 @@
     </head>
     <body @if (BaseHelper::siteLanguageDirection() == 'rtl') dir="rtl" @endif>
         {!! apply_filters(THEME_FRONT_BODY, null) !!}
-        <header>Header</header>
+
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container align-items-end">
+                    <a class="link-hailongglass" href="#" title="HAILONGGLASS">
+                        <div class="box-symbol">
+                            <span> HAILON GLASS </span>
+                            <span> VSGLASS </span>
+                        </div>
+                        <h1> KINH AN TOAN HAI LONG - VIET NHAT </h1>
+                    </a>
+
+                    <div class="collapse navbar-collapse align-items-end" id="navbarTogglerDemo01">
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
