19129fbb5d869e1feee0c1566dc16b3929e7fe2f
ThuanNguyen <nguyenbathuan98@gmail.com>
2021-07-13 13:54:05 +0700

    project

---
+++ platform/themes/hailongglass/views/components/pagination.blade.php
@@ -0,0 +1,23 @@
+<section>
+    <div class="container-customize">
+        <div class="pagination-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="pagination_style">
+                        <nav aria-label="...">
+                            <ul class="pagination">
+                                {{-- <li class="disabled page-link-last"><span>Trang trước</span></li> --}}
+                                <li class="active page-item"><span class="page-link">1</span>
+                                    <li class=" page-item"><span class="page-link">2</span></li>
+                                    <li class=" page-item"><span class="page-link">3</span></li>
+                                    <li class=" page-item"><span class="page-link">4</span></li>
+                                <li class="page-link-last"><a href="" rel="next">Trang sau</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+</section>
