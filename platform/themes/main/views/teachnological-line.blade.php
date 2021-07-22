daba79ea7dadaa46ce7ed00c40d160ed4cb0940c
Quốc Bình <tranaiquoc4499@gmail.com>
2021-07-22 10:11:36 +0700

    Profile - Dây chuyền công nghê chính

--- platform/themes/main/views/teachnological-line.blade.php
+++ platform/themes/main/views/teachnological-line.blade.php
@@ -35,14 +35,29 @@
                                         <h3 class="__content__title">{!! has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : "" !!}</h3>
                                         {!! has_sub_field($item, 'mo_ta') ? get_sub_field($item, 'mo_ta') : "" !!}
                                     </div>
-                                    <button class="btn-read-more bg-white" data-toggle="modal" data-target="#exampleModalCenter" href="#" title="{!! __('Details') !!}">
+                                    <button class="btn-read-more bg-white" data-toggle="modal" data-target="#technical_line_{{ \Illuminate\Support\Str::slug(has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : '', '_') }}" href="#" title="{!! __('Details') !!}">
                                         {!! __('Details') !!}
                                     </button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade dccn" id="technical_line_{{ \Illuminate\Support\Str::slug(has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : '', '_') }}" tabindex="-1" role="dialog" aria-labelledby="technical_line_{{ \Illuminate\Support\Str::slug(has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : '', '_') }}" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLongTitle"> {{ has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : "" }} </h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <i class="fal fa-times"></i>
+                                                    </button>
+                                                </div>
+                                                {!! has_sub_field($item, 'chi_tiet') ? get_sub_field($item, 'chi_tiet') : "" !!}
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
 
                                 <div class="section-teachnological__row__col order-sm-2 order-1 mb-sm-0 mb-3 __picture">
                                     @if(has_sub_field($item, 'hinh_anh'))
-                                        <img width="640" height="360" class="w-100" src="{{get_image_url(get_sub_field($item, 'hinh_anh'))}}" alt="{{ has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : "" }}" />
+                                        <img width="640" height="360" class="w-100" src="{{get_image_url(get_sub_field($item, 'hinh_anh'))}}" alt="{{ has_sub_field($item, 'tieu_de') ? get_sub_field($item, 'tieu_de') : '' }}" />
                                     @endif
                                 </div>
                             </div>
@@ -74,69 +89,4 @@
         </div>
     </div>
     <!--end css in file common.scss----->
-</div>
-
-<!-- Modal -->
-<div class="modal fade dccn" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle"> Phân xưởng kính dán </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <i class="fal fa-times"></i>
-                </button>
-            </div>
-
-            <table>
-                <tr>
-                    <th> <i>Tên máy</i> </th>
-                    <th> <i>Số lượng</i> </th>
-                    <th> <i>Bộ phận</i> </th>
-                    <th> </th>
-                </tr>
-                <tr>
-                    <td>Dây chuyền dán kính</td>
-                    <td>03</td>
-                    <td>03</td>
-                </tr>
-                <tr>
-                    <td>Máy ép kính</td>
-                    <td>04</td>
-                    <td>04</td>
-                    <td rowspan="4">Tổ ÔLây</td>
-                </tr>
-                <tr>
-                    <td>Nồi hấp kính 2600 x 6000</td>
-                    <td>01</td>
-                    <td>01</td>
-                </tr>
-                <tr>
-                    <td>Nồi hấp kính 3000 x 8000 (autoclave)</td>
-                    <td>02</td>
-                    <td>02</td>
-                </tr>
-                <tr>
-                    <td>Nồi hấp kính AC 250 - 500</td>
-                    <td>01</td>
-                    <td>01</td>
-                </tr>
-                <tr>
-                    <td>Máy phun cát</td>
-                    <td>02</td>
-                    <td>02</td>
-                    <td>Tô Phun cát</td>
-                </tr>
-                <tr>
-                    <td>Cầu trục 5 tấn, 3 tấn, 1 tấn, 2 tấn</td>
-                    <td>13</td>
-                    <td colspan="2">13</td>
-                </tr>
-                <tr>
-                    <td>Máy phát điện</td>
-                    <td>01</td>
-                    <td colspan="2">01</td>
-                </tr>
-            </table>
-        </div>
-    </div>
 </div>
