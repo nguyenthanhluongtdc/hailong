8c11ee7caea863e5b75123526bfa3dd4e2ee2cd7
Eugene <eugene@visualweber.com>
2021-07-22 11:56:29 +0700

    no message

---
+++ platform/plugins/payment/src/Models/Payment.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Platform\Payment\Models;
+
+use Platform\ACL\Models\User;
+use Platform\Base\Models\BaseModel;
+use Platform\Base\Traits\EnumCastable;
+use Platform\Payment\Enums\PaymentMethodEnum;
+use Platform\Payment\Enums\PaymentStatusEnum;
+use Html;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Payment extends BaseModel
+{
+    use EnumCastable;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'payments';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'amount',
+        'currency',
+        'user_id',
+        'charge_id',
+        'payment_channel',
+        'description',
+        'status',
+        'order_id',
+        'payment_type',
+        'customer_id',
+        'customer_type',
+        'refunded_amount',
+        'refund_note',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'payment_channel' => PaymentMethodEnum::class,
+        'status'          => PaymentStatusEnum::class,
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault();
+    }
+
+    /**
+     * @return MorphTo
+     */
+    public function customer(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        $time = Html::tag('span', $this->created_at->diffForHumans(), ['class' => 'small italic']);
+
+        return 'You have created a payment #' . $this->charge_id . ' via ' . $this->payment_channel->label() . ' ' . $time .
+            ': ' . number_format($this->amount, 2, '.', ',') . $this->currency;
+    }
+}
