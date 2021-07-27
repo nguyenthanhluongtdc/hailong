034799a5ea9ef3d07b6217eec50c0ce83376fcfe
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-07-27 17:03:29 +0700

    Revert "fix footer"
    This reverts commit 5764690145fcda55b7822d0730cc1b4407ee6cdf.

--- platform/themes/main/views/index.blade.php
+++ platform/themes/main/views/index.blade.php
@@ -384,17 +384,16 @@
 
 @includeIf("theme.main::views.components.modal-google-map");
 
-@if(theme_option('image_popup'))
+<!-- use this for popup-->
 <div id="boxes">
   <div style="display: none;" id="dialog" class="window">
     <div id="lorem">
         <button class="close popup"> <i class="fal fa-times"></i> </button>
-      <img class="mw-100 h-auto" src="{{rvMedia::getImageUrl(theme_option('image_popup'))}}"/>
+      <img class="mw-100 h-auto" src="{{Theme::asset()->url('images/home/popup.jpg')}}"/>
     </div>
   </div>
   <div id="mask"></div>
 </div>
-@endif
 
 <script>
 
