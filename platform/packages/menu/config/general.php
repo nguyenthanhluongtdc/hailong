172cbb284e156f6cb2233b0de327fd11986007aa
Eugene <eugene@visualweber.com>
2021-08-07 15:15:03 +0700

    no message

--- platform/packages/menu/config/general.php
+++ platform/packages/menu/config/general.php
@@ -5,12 +5,6 @@ return [
         'header-menu' => 'Header Navigation',
         'main-menu'   => 'Main Navigation',
         'footer-menu' => 'Footer Navigation',
-        'project-categories-menu' => 'Projects menu',
-        'introduce-menu' => "Introduce menu",
-        'products-menu' => "Products menu",
-        'footer-column-one-menu' => "Footer column one menu",
-        'footer-column-two-menu' => "Footer column two menu",
-        'footer-column-three-menu' => "Footer column three menu",
     ],
     'cache'     => [
         'enabled' => env('CACHE_FRONTEND_MENU', false),
--- platform/packages/page/resources/views/themes/page.blade.php
+++ platform/packages/page/resources/views/themes/page.blade.php
@@ -1,12 +1,7 @@
-{{-- <div class="container">
+<div class="container">
     <h3 class="page-intro__title">{{ $page->name }}</h3>
     {!! Theme::breadcrumb()->render() !!}
 </div>
 <div>
-    {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content, 'youtube'), $page) !!}
-</div> --}}
-@includeIf('theme.main::views.components.tabs-banner')
-
-<div class="container-customize py-5">
     {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, clean($page->content, 'youtube'), $page) !!}
 </div>
