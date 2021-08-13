dc6b22f37daa30062285fbc64e20acbb4ee7c015
luong.nt <ntl.tdc.edu.vn@gmail.com>
2021-08-13 17:23:05 +0700

    fix

--- platform/themes/main/views/index.blade.php
+++ platform/themes/main/views/index.blade.php
@@ -324,8 +324,7 @@
                     @php
                         $posts = get_latest_posts(3);
                     @endphp
-
-                    @if(!empty($posts))
+                    @if(count($posts)>0)
                         <div class="col-lg-6" data-aos="fade-right">
                             <div class="col-core col-big h-100">
                                 <a href="{{$posts[0]->url}}" title="{{$posts[0]->name}}" class="h-100 w-100 d-lg-inline-block">
